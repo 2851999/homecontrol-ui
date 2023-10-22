67660e0289e6489f5a3382d933615e5122b102b9
2851999 <joelvdavies@btinternet.com>
2023-10-22 11:33:49 +0100

    Save settings store to local storage

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -18,19 +18,31 @@ const makeStore = (preloadedState?: PreloadedState<RootState>) =>
     preloadedState: preloadedState,
   });
 
+export const loadFromLocalStorage = (): PreloadedState<RootState> | null => {
+  const serialisedState = localStorage.getItem("settings");
+  if (serialisedState !== null) {
+    return { settings: JSON.parse(serialisedState) };
+  } else return null;
+};
+
+export const saveToLocalStorage = (state: RootState) => {
+  const serialisedState = JSON.stringify(state.settings);
+  localStorage.setItem("settings", serialisedState);
+};
+
 export const useStore = () => {
   const [store, setStore] = useState(makeStore());
 
   useEffect(() => {
     // In here can guarantee on client
-    setStore(
-      makeStore({
-        settings: {
-          themeMode:
-            (localStorage.getItem("themeMode") as ThemeMode) || "light",
-        },
-      })
-    );
+    const newStore = makeStore({
+      settings: {
+        themeMode: (localStorage.getItem("themeMode") as ThemeMode) || "light",
+      },
+    });
+
+    newStore.subscribe(() => saveToLocalStorage(newStore.getState()));
+    setStore(newStore);
   }, []);
 
   return store;
