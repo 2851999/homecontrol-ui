46ed4cf89a11aaf3cbf7799331e7649e0763ec93
2851999 <joelvdavies@btinternet.com>
2024-03-05 20:35:47 +0000

    Upgrade packages

--- src/components/rooms/ControllerAccordion.tsx
+++ src/components/rooms/ControllerAccordion.tsx
@@ -61,18 +61,16 @@ type TooltipToggleButtonProps = ToggleButtonProps & {
   TooltipProps: Omit<TooltipProps, "children">;
 };
 
-// eslint-disable-next-line react/display-name
-const TooltipToggleButton: React.FC<TooltipToggleButtonProps> = forwardRef(
-  ({ TooltipProps, ...props }, ref) => {
+export const TooltipToggleButton: React.FC<TooltipToggleButtonProps> =
+  // eslint-disable-next-line react/display-name
+  forwardRef(({ TooltipProps, ...props }, ref) => {
     return (
       <Tooltip {...TooltipProps}>
         <ToggleButton ref={ref} {...props} />
       </Tooltip>
     );
-  }
-);
-
-export default TooltipToggleButton;
+    // https://github.com/mui/material-ui/issues/32420
+  }) as React.FC<TooltipToggleButtonProps>;
 
 interface ControllerAccordionACProps {
   controller: ControllerAC;
